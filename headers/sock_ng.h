a17c296c6e9892a4af369928f127e703d7162dfe
UserX <icedebian@gmail.com>
2012-07-15 21:14:58 -0300

    modified:   MacGyver_server.c 	modified:   UnderConstruction/Multiplex_server/client.c 	modified:   UnderConstruction/Multiplex_server/mp_server.c 	modified:   UnderConstruction/server_construction.c 	modified:   authlist/auth.c 	modified:   headers/color_rset.h 	modified:   headers/dump_io.h 	modified:   headers/exceptions.h 	modified:   headers/shell_fnct.h 	modified:   headers/sock_ng.h 	modified:   headers/userList.h

--- headers/sock_ng.h
+++ headers/sock_ng.h
@@ -1,6 +1,24 @@
-//**************************************************************
-// File: sock_ng.h {ng seems like new generation} :: By UserX ::
-//**************************************************************
+/***************************************************************************
+* File: sockng.h                                                           *
+* Copyright 15/07/2012 UserX <userx@gmail.com>                             *
+*                                                                          *
+* This program is free software; you can redistribute it and/or modify     *
+* it under the terms of the GNU General Public License as published by     *
+* the Free Software Foundation; either version 2 of the License, or        *
+* (at your option) any later version.                                      *
+*                                                                          *
+* This program is distributed in the hope that it will be useful,          *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+* GNU General Public License for more details.                             *
+*                                                                          *
+* You should have received a copy of the GNU General Public License        *
+* along with this program; if not, write to the Free Software              *
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,               *
+* MA 02110-1301, USA.                                                      *
+*                                                                          *
+*                                                                          *
+***************************************************************************/
 
 #include <sys/socket.h>
 #include <netinet/in.h>
